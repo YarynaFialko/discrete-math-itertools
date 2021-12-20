6cfc6bdcf5dd6b7c407aea2029b36847de00a9d6
YarynaFialko <92580927+YarynaFialko@users.noreply.github.com>
2021-12-20 22:19:48 +0200

    Create yaryna.py

---
+++ yaryna.py
@@ -0,0 +1,14 @@
+def count(start=0, step=1):
+    i = 0
+    while True:
+        yield start + i*step
+        i += 1
+
+def cycle(iterable):
+    while True:
+        for element in iterable:
+            yield element
+
+def repeat(value):
+    while True:
+        yield value
